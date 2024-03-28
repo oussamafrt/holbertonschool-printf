75a60a19b07fe326ed24e8056fe872495bd882e3
NamaKa298 <marion.saint-martin@protonmail.com>
2024-03-28 11:19:39 +0100

    rajout du pourcent espace

--- printf.c
+++ printf.c
@@ -20,6 +20,10 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_start(args, format);
+	if (format == NULL || (*format == '%' && *format++ == '\0'))
+	{
+		return (-1);
+	}
 	while (*format)
 	{
 		if (*format != '%')
